0b934ad491195139b10c266dbf5159d0b0d5cbf5
moopet <ben@moopet.net>
2011-06-05 20:37:20 +0100

    All methods now take explicit arguments rather than dictionaries

--- canopy.py
+++ canopy.py
@@ -35,7 +35,7 @@ class ForrstAPI():
             if self.token:
                 data['access_token'] = self.token
             elif self.username and self.password:
-                data['username'] = self.username
+                data['email_or_username'] = self.username
                 data['password'] = self.password
             else:
                 raise ForrstAPIError('authentication required')
@@ -68,15 +68,36 @@ class ForrstAPI():
         """
         return self.call_api('users/auth', authenticate=True)
 
-    def users_info(self, data={}, authenticate=False):
+    def users_info(self, username=None, id=None, data={}, authenticate=False):
         """Return user info."""
+        if username:
+            data['username'] = username
+        elif id:
+            data['id'] = id
+        else:
+            raise ForrstAPIError('missing argument: expected username or id')
         return self.call_api('users/info', data=data, authenticate=authenticate)
 
-    def user_posts(self, data={}, authenticate=False):
+    def user_posts(self, username=None, id=None, post_type= None, limit=None,
+                   after=None, data={}, authenticate=False):
         """Return a user's posts."""
-        return self.call_api('user/posts', data=data, authenticate=authenticate)
+        if username:
+            data['username'] = username
+        elif id:
+            data['id'] = id
+        else:
+            raise ForrstAPIError('missing argument: expected username or id')
+        if post_type:
+            data['type'] = post_type
+        if limit:
+            data['limit'] = limit
+        if after:
+            data['after'] = after
+        if self.call_api('user/posts', data=data, authenticate=authenticate):
+            return self.response.get('posts')
+        return False
 
-    def posts_show(self, data={}, authenticate=False):
+    def posts_show(self, id=None, tiny_id=None, data={}, authenticate=False):
         """Return data about a single post.
         Note: For questions, content is the question.
         For code, content contains the code snippet.
@@ -84,12 +105,39 @@ class ForrstAPI():
         it is not used for questions.
 
         """
+        if id:
+            data['id'] = id
+        if tiny_id:
+            data['tiny_id'] = tiny_id
         return self.call_api('users/info', data=data, authenticate=authenticate)
 
-    def posts_list(self, data={}, authenticate=False):
-        """Return a list of posts of a given type."""
-        return self.call_api('posts/list', data=data, authenticate=authenticate)
+    def posts_all(self, after=None, data={}, authenticate=False):
+        """Return a list of all posts in reverse-chron order"""
+        if after:
+            data['after'] = after
+        if self.call_api('posts/all', data=data, authenticate=authenticate):
+            return self.response.get('posts')
+        return False
 
-    def post_comments(self, data={}):
-        """Returns a post's comments."""
-        return self.call_api('post/comments', data=data, authenticate=True)
+    def posts_list(self, post_type=None, sort=None, page=None, data={},
+                   authenticate=False):
+        """Return a list of posts of a given type."""
+        if post_type:
+            data['post_type'] = post_type
+        if sort:
+            data['sort'] = sort
+        if page:
+            data['page'] = page
+        if self.call_api('posts/list', data=data, authenticate=authenticate):
+            return self.response.get('posts')
+        return False
+
+    def post_comments(self, id=None, tiny_id=None, data={}):
+        """Return a post's comments."""
+        if id:
+            data['id'] = id
+        if tiny_id:
+            data['tiny_id'] = tiny_id
+        if self.call_api('post/comments', data=data, authenticate=True):
+            return self.response.get('comments')
+        return False
